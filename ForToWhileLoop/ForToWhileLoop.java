fdb7a939d8d2d478a0ef0a0ac87b1f376e20c982
BraidenB <56049824+BraidenB@users.noreply.github.com>
2019-10-01 15:00:15 -0700

    Add files via upload

---
+++ ForToWhileLoop/ForToWhileLoop.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+class ForToWhileLoop {
+    public static void main (String[] args) {
+        int i = 100;
+        while (i >= 10) {
+            System.out.println("i is: " + i);
+            i--;
+        }
+    }
+}
